ede89d65f1cd19b2724a8e79d71c8cc46c48ba7f
Akhilesh Yarabarla <akhileshyarabarla@gmail.com>
2015-06-04 16:57:03 -0500

    Added some comments

--- 3.py
+++ 3.py
@@ -6,21 +6,22 @@ read_arr = list(ob_read)
 
 word = []
 
-def for_loop():
+def for_loop(): # Loops through array to find solution
 	for i in range(len(read_arr)):
 
 		if (i + 8) > len(read_arr): # To keep index in bounds
 			break
 
 		if not(read_arr[i]).isupper() and (read_arr[i + 1]).isupper() and (read_arr[i + 2]).isupper() and (read_arr[i + 3]).isupper() and(read_arr[i + 4]).islower() and (read_arr[i + 5]).isupper() and (read_arr[i + 6]).isupper() and (read_arr[i + 7]).isupper() and not(read_arr[i + 8]).isupper(): 
+		
 			word.append(read_arr[i + 4])
 
 	print "".join(word)
 
 
-def reg_ex():
+def reg_ex(): # Uses regex to find the pattern
 	print "".join( re.findall("[^A-Z][A-Z]{3}([a-z])[A-Z]{3}[^A-Z]", ob_read))
 
 
-
-reg_ex()
+# for_loop()
+# reg_ex() 
