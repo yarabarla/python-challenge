5e2e5ac51a64dfda597c01d4781d3c53d2d3cf41
Akhilesh Yarabarla <akhileshyarabarla@gmail.com>
2015-06-04 17:17:48 -0500

    Solved using regex as well

--- 2.py
+++ 2.py
@@ -1,13 +1,24 @@
-import sys
+import sys, re
 
 file_ob = open("2.dat", 'r')
 file_read = file_ob.read()
 
 file_list = list(file_read)
 
+def for_loop():
 
-for i in range(len(file_list)):
-	if (file_list[i]).isalpha():
-		sys.stdout.write(file_list[i])
+	for i in range(len(file_list)):
+		if (file_list[i]).isalpha():
+			sys.stdout.write(file_list[i]) # Needed so all the letters print on the same line.
 
-print '\n'
+	print '\n'
+
+
+def reg_ex():
+
+	pattern = re.compile("[a-z]", re.I) # I stands for IGNORECASE
+	print "".join(pattern.findall(file_read))
+
+
+# for_loop()
+# reg_ex()
