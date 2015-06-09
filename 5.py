4c668eedb09daa409a80559e298586b3e6dd8e64
Akhilesh Yarabarla <akhileshyarabarla@gmail.com>
2015-06-09 16:25:09 -0500

    All tuples are parsed as strings and are added to an array

--- 5.py
+++ 5.py
@@ -5,6 +5,13 @@ import requests, pickle
 input_url = "http://www.pythonchallenge.com/pc/def/banner.p"
 obj = requests.get(input_url)
 text = obj.text
-
 banner = pickle.loads(text)
-print banner
+
+final = []
+
+for index, value in enumerate(banner):
+    line = []
+    for j in value:
+        line.append(j[0] * j[1])
+        final.append("".join(line))
+    final.append('\n')
