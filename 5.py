6848ca62a73308875ed720cb27347d9e7fb373b9
Akhilesh Yarabarla <akhileshyarabarla@gmail.com>
2015-06-09 15:59:48 -0500

    Unpickled banner.p

--- 5.py
+++ 5.py
@@ -1,10 +1,10 @@
 # http://www.pythonchallenge.com/pc/def/peak.html
 
-import requests
+import requests, pickle
 
 input_url = "http://www.pythonchallenge.com/pc/def/banner.p"
 obj = requests.get(input_url)
-banner = obj.text
+text = obj.text
 
+banner = pickle.loads(text)
 print banner
-
