b710a7d27a3067e5dd4b1cdbc394b1afb1be6708
Akhilesh Yarabarla <akhileshyarabarla@gmail.com>
2015-06-09 15:00:59 -0500

    Finished program

--- 4.py
+++ 4.py
@@ -2,13 +2,23 @@
 
 import requests, re
 
-nothing = 12345 # Initial nothing value
+nothing = 63579 # Initial nothing value, prompted to change during program
 url =  "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=%d"
 
-req = requests.get(url % nothing) 
-data = req.text # Stores body of text to be parsed
+while True:
+    req = requests.get(url % nothing)
+    data = req.text # Stores body of text to be parsed
+    pattern = re.compile('\d+')
 
-pattern = re.compile('\d+')
-nums = pattern.findall(data)
-nothing = int("".join(nums))
-print nothing
+    try:
+        nums = pattern.findall(data)
+        nothing = int("".join(nums))
+        print data #To read the prompts that occasionally show up
+
+    except:
+        break
+
+
+print "Data is: %s and nothing is %d" % (data, nothing)
+
+
