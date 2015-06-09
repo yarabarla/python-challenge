aff7d26cfa830191e6d940f35052716b11f64d9a
Akhilesh Yarabarla <akhileshyarabarla@gmail.com>
2015-06-09 15:47:44 -0500

    Got banner file

--- 5.py
+++ 5.py
@@ -1,3 +1,10 @@
 # http://www.pythonchallenge.com/pc/def/peak.html
 
+import requests
+
+input_url = "http://www.pythonchallenge.com/pc/def/banner.p"
+obj = requests.get(input_url)
+banner = obj.text
+
+print banner
 
