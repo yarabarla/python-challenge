7288260c6373bfc9e4a24101fe7a75aaf265f015
Akhilesh Yarabarla <akhileshyarabarla@gmail.com>
2015-06-08 23:37:08 -0500

    Used regex to get number

--- 4.py
+++ 4.py
@@ -5,5 +5,10 @@ import requests, re
 nothing = 12345 # Initial nothing value
 url =  "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=%d"
 
-req = requests.get(url % nothing)
-print req.text
+req = requests.get(url % nothing) 
+data = req.text # Stores body of text to be parsed
+
+pattern = re.compile('\d+')
+nums = pattern.findall(data)
+nothing = int("".join(nums))
+print nothing
